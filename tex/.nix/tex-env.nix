9f8fc69dca9860c42a51de8d1bc49270fdc15424
jfjhh <alex.striff1@gmail.com>
2021-08-13 00:45:45 -0400

    Add tex2nix pre-commit hook

--- tex/.nix/tex-env.nix
+++ tex/.nix/tex-env.nix
@@ -2,10 +2,28 @@
 { texlive, extraTexPackages ? {} }:
 (texlive.combine ({
     inherit (texlive) scheme-small;
-    "siunitx" = texlive."siunitx";
+    "minted" = texlive."minted";
     "physics" = texlive."physics";
-    "kantlipsum" = texlive."kantlipsum";
-    "amsmath" = texlive."amsmath";
+    "xstring" = texlive."xstring";
+    "fvextra" = texlive."fvextra";
+    "kvsetkeys" = texlive."kvsetkeys";
     "mathtools" = texlive."mathtools";
+    "ltxcmds" = texlive."ltxcmds";
+    "kantlipsum" = texlive."kantlipsum";
+    "finstrut" = texlive."finstrut";
+    "xcolor" = texlive."xcolor";
+    "framed" = texlive."framed";
+    "fancyvrb" = texlive."fancyvrb";
+    "lineno" = texlive."lineno";
+    "etexcmds" = texlive."etexcmds";
+    "etoolbox" = texlive."etoolbox";
+    "catchfile" = texlive."catchfile";
+    "ifplatform" = texlive."ifplatform";
+    "kvoptions" = texlive."kvoptions";
+    "upquote" = texlive."upquote";
+    "float" = texlive."float";
+    "siunitx" = texlive."siunitx";
+    "amsmath" = texlive."amsmath";
+    "pdftexcmds" = texlive."pdftexcmds";
 
 } // extraTexPackages))
